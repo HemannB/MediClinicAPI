a8baa972fce041d22b7b17c924fdea2624c41d51
HemannB <brunohemann@gmail.com>
2026-07-05 23:26:26 -0300

    chore: initial project setup
    Signed-off-by: HemannB <brunohemann@gmail.com>

---
+++ src/main/java/com/example/mediclinicapi/MediClinicApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.mediclinicapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediClinicApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MediClinicApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mediclinicapi/MediClinicApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mediclinicapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediClinicApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
