1905f15f2ef2a9e5637837eca92e1f8d101bfce5
HemannB <brunohemann@gmail.com>
2026-07-12 22:08:30 -0300

    feat(security): add auth0 jwt dependency and implement TokenService generator
    Signed-off-by: HemannB <brunohemann@gmail.com>

---
+++ src/main/java/com/example/mediclinicapi/service/TokenService.java
@@ -0,0 +1,36 @@
+package com.example.mediclinicapi.service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.example.mediclinicapi.domain.User;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+    @Value("${api.security.token.secret:secret-key-teste-123-123}")
+    private String secret;
+
+    public String generateToken(User user) {
+        try{
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.create()
+                    .withIssuer("medclinic-api")
+                    .withSubject(user.getEmail())
+                    .withClaim("role",user.getRole().name())
+                    .withExpiresAt(expirationDate())
+                    .sign(algorithm);
+        } catch (JWTCreationException exception) {
+            throw new RuntimeException("Erro ao gerar token JWT", exception);
+        }
+    }
+    private Instant expirationDate() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+
+}
