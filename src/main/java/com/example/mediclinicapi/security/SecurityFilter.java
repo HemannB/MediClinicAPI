0d6f497424780bc03356bd5e179c12735d21cf7d
HemannB <brunohemann@gmail.com>
2026-07-12 22:32:42 -0300

    feat(security): implement SecurityFilter to intercept and validate JWT tokens
    Signed-off-by: HemannB <brunohemann@gmail.com>

---
+++ src/main/java/com/example/mediclinicapi/security/SecurityFilter.java
@@ -0,0 +1,52 @@
+package com.example.mediclinicapi.security;
+
+import com.example.mediclinicapi.repository.UserRepository;
+import com.example.mediclinicapi.service.TokenService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class SecurityFilter extends OncePerRequestFilter {
+
+    private final TokenService tokenService;
+    private final UserRepository userRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String tokenJWT = recoverToken(request);
+
+        if (tokenJWT != null) {
+            String subject = tokenService.validateToken(tokenJWT);
+
+            if (!subject.isEmpty()) {
+                UserDetails user = userRepository.findByEmail(subject)
+                        .orElseThrow(() -> new RuntimeException("Usuário não encontrado"));
+
+                var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String recoverToken(HttpServletRequest request) {
+        String authorizationHeader = request.getHeader("Authorization");
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            return authorizationHeader.replace("Bearer ", "");
+        }
+        return null;
+    }
+}
