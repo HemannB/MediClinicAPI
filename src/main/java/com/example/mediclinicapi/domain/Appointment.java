a6214afa20110882eb7cb8b685fcc6c1dbcafb48
HemannB <brunohemann@gmail.com>
2026-07-19 23:19:35 -0300

    feat(appointment): link appointments to patients
    Signed-off-by: HemannB <brunohemann@gmail.com>

--- src/main/java/com/example/mediclinicapi/domain/Appointment.java
+++ src/main/java/com/example/mediclinicapi/domain/Appointment.java
@@ -7,7 +7,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Entity
@@ -22,6 +21,10 @@ public class Appointment {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "doctor_id", nullable = false)
     private User doctor;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "patient_id", nullable = false)
+    private Patient patient;
 
     @Column(nullable = false)
     private LocalDateTime dateTime;
