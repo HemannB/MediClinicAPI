34d0ee146e252b835a220b522b1977ce2e78087b
HemannB <brunohemann@gmail.com>
2026-07-19 23:45:40 -0300

    feat(user): find users by role
    Signed-off-by: HemannB <brunohemann@gmail.com>

--- src/main/java/com/example/mediclinicapi/repository/UserRepository.java
+++ src/main/java/com/example/mediclinicapi/repository/UserRepository.java
@@ -1,9 +1,14 @@
 package com.example.mediclinicapi.repository;
 
 import com.example.mediclinicapi.domain.User;
+import com.example.mediclinicapi.domain.enums.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
+
+    List<User> findByRole(Role role);
 }
