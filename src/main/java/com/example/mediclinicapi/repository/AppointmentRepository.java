fa8fcf5a83beeb1637dd36c74f4e5aaca8309637
HemannB <brunohemann@gmail.com>
2026-07-19 23:57:46 -0300

    feat(appointment): query appointments by participant
    Signed-off-by: HemannB <brunohemann@gmail.com>

--- src/main/java/com/example/mediclinicapi/repository/AppointmentRepository.java
+++ src/main/java/com/example/mediclinicapi/repository/AppointmentRepository.java
@@ -5,10 +5,15 @@ import com.example.mediclinicapi.domain.enums.AppointmentStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 
 public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
     Boolean existsByDoctorIdAndDateTimeAndStatusNot(Long doctorId, LocalDateTime dateTime, AppointmentStatus status);
 
     Boolean existsByPatientIdAndDateTimeAndStatusNot(Long patientId, LocalDateTime dateTime, AppointmentStatus status);
+
+    List<Appointment> findByDoctorId(Long doctorId);
+
+    List<Appointment> findByPatientId(Long patientId);
 }
